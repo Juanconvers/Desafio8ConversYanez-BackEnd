5b40dd57fd15508673f3a2ead1dc01873d4606c9
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2024-05-21 15:07:56 -0500

    Commit No. 3: Implementación del modelo MVC completo en el proyecto.

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -2,7 +2,8 @@ import cartRouter from './cartRouter.js'
 import productsRouter from './productsRouter.js'
 import userRouter from './userRouter.js'
 import chatRouter from './chatRouter.js'
-import upload from '../config/multer.js'
+import sessionRouter from './sessionRouter.js'
+import multerRouter from './multerRouter.js'
 import express from 'express'
 import { __dirname } from '../path.js'
 
@@ -11,21 +12,14 @@ const indexRouter = express.Router()
 
 //Routes
 indexRouter.get('/', (req, res) => {
-    res.status(200).send("Bienvenido/a!")
+    res.status(200).send("Bienvenido!")
 })
 indexRouter.use('/public', express.static(__dirname + '/public'))
+indexRouter.use('/upload', multerRouter)
 indexRouter.use('/api/products', productsRouter, express.static(__dirname + '/public'))
 indexRouter.use('/api/cart', cartRouter)
 indexRouter.use('/api/chat', chatRouter, express.static(__dirname + '/public'))
 indexRouter.use('/api/users', userRouter)
-
-indexRouter.post('/upload', upload.single('product'), (req, res) => {
-    try {
-        console.log(req.file)
-        res.status(200).send("Imagen cargada correctamente")
-    } catch (e) {
-        res.status(500).send("Error al cargar imagen")
-    }
-})
+indexRouter.use('/api/session', sessionRouter)
 
 export default indexRouter
